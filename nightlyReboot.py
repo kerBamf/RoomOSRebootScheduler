e194d2f99f61acc184aac71a84082c474a27a161
kerBamf <zach.pedigo@gmail.com>
2024-05-17 17:23:43 -0400

    Adjusted naming conventions for more accurate description when initiating automatic reboots

--- nightlyReboot.py
+++ nightlyReboot.py
@@ -44,7 +44,7 @@ DEFAULT_PASSWORD = 'Basic ' + PASSWORD
 def reboot_request(auth_val=DEFAULT_PASSWORD, ip=None, idx=None, file=None):
     url = f"https://{ip}/putxml"
     cell_no = idx+2
-    payload = "<Command>\r\n\t<Standby>\r\n\t\t<Deactivate></Deactivate>\r\n\t</Standby>\r\n\t<UserInterface>\r\n\t\t<Message>\r\n\t\t\t<Alert>\r\n\t\t\t\t<Display>\r\n\t\t\t\t\t<Duration>10</Duration>\r\n\t\t\t\t\t<Text>Use touchpanel to cancel reboot</Text>\r\n\t\t\t\t\t<Title>NIGHTLY REBOOT INITIATED</Title>\r\n\t\t\t\t</Display>\r\n\t\t\t</Alert>\r\n\t\t</Message>\r\n\t</UserInterface>\r\n</Command>"
+    payload = "<Command>\r\n\t<Standby>\r\n\t\t<Deactivate></Deactivate>\r\n\t</Standby>\r\n\t<UserInterface>\r\n\t\t<Message>\r\n\t\t\t<Alert>\r\n\t\t\t\t<Display>\r\n\t\t\t\t\t<Duration>10</Duration>\r\n\t\t\t\t\t<Text>Use touchpanel to cancel reboot</Text>\r\n\t\t\t\t\t<Title>AUTOMATED REBOOT INITIATED</Title>\r\n\t\t\t\t</Display>\r\n\t\t\t</Alert>\r\n\t\t</Message>\r\n\t</UserInterface>\r\n</Command>"
 
     headers = {'Content-Type': 'text/xml','Authorization': auth_val}
     try:
