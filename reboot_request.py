7dc6fc83c3a059cc63fc50300c30a988edfb92c0
kerBamf <zach.pedigo@gmail.com>
2024-06-06 17:24:22 -0400

    Further adjusted reboot_request.py to allow for function to be broken out as a module

--- reboot_request.py
+++ reboot_request.py
@@ -3,9 +3,8 @@ import requests
 from urllib3.exceptions import InsecureRequestWarning
 
 DEFAULT_PASSWORD = environ.get('PASSWORD')
-codec_ip = input('Enter codec ip: ')
 
-def reboot_request(auth_val=DEFAULT_PASSWORD, ip=codec_ip, idx=None, file=None):
+def reboot_request(auth_val=DEFAULT_PASSWORD, ip='', idx=None, file=None):
     url = f"http://{ip}/putxml"
     payload = "<Command>\r\n\t<Standby>\r\n\t\t<Deactivate></Deactivate>\r\n\t</Standby>\r\n\t<UserInterface>\r\n\t\t<Message>\r\n\t\t\t<Alert>\r\n\t\t\t\t<Display>\r\n\t\t\t\t\t<Duration>10</Duration>\r\n\t\t\t\t\t<Text>Use touchpanel to cancel reboot</Text>\r\n\t\t\t\t\t<Title>AUTOMATED REBOOT INITIATED</Title>\r\n\t\t\t\t</Display>\r\n\t\t\t</Alert>\r\n\t\t</Message>\r\n\t</UserInterface>\r\n</Command>"
 
@@ -18,4 +17,5 @@ def reboot_request(auth_val=DEFAULT_PASSWORD, ip=codec_ip, idx=None, file=None):
         print(err.response)
 
 if __name__ == "__main__":
-    reboot_request()
+    codec_ip = input('Enter codec ip: ')
+    reboot_request(ip=codec_ip)
