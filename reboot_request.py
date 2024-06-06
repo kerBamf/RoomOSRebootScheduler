43637759beb4fe642331abb2a804c76b644b2a52
kerBamf <zach.pedigo@gmail.com>
2024-06-06 17:20:14 -0400

    Rebuilt reboot_request.py to allow for standalone reboot command testing

--- reboot_request.py
+++ reboot_request.py
@@ -1,5 +1,21 @@
-from weekly_reboot import reboot_request
+from os import environ
+import requests
+from urllib3.exceptions import InsecureRequestWarning
 
+DEFAULT_PASSWORD = environ.get('PASSWORD')
 codec_ip = input('Enter codec ip: ')
 
-reboot_request(ip=codec_ip)
+def reboot_request(auth_val=DEFAULT_PASSWORD, ip=codec_ip, idx=None, file=None):
+    url = f"http://{ip}/putxml"
+    payload = "<Command>\r\n\t<Standby>\r\n\t\t<Deactivate></Deactivate>\r\n\t</Standby>\r\n\t<UserInterface>\r\n\t\t<Message>\r\n\t\t\t<Alert>\r\n\t\t\t\t<Display>\r\n\t\t\t\t\t<Duration>10</Duration>\r\n\t\t\t\t\t<Text>Use touchpanel to cancel reboot</Text>\r\n\t\t\t\t\t<Title>AUTOMATED REBOOT INITIATED</Title>\r\n\t\t\t\t</Display>\r\n\t\t\t</Alert>\r\n\t\t</Message>\r\n\t</UserInterface>\r\n</Command>"
+
+    headers = {'Content-Type': 'text/xml','Authorization': f'Basic {auth_val}'}
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        response.raise_for_status()
+        print(response)
+    except requests.exceptions.HTTPError as err:
+        print(err.response)
+
+if __name__ == "__main__":
+    reboot_request()
